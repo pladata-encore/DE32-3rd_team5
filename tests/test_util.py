d803d2d93d20e0efb19d934256a881c9161ceba7
Jeonghoon2 <ukidd12@naver.com>
2024-10-08 16:32:31 +0900

    .

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,8 @@
-from de32_3rd_team5.geoutil import loc_trans
+from de32_3rd_team5.utils.geoutil import loc_trans
+
 
 def test_loc_trans():
-	location = '36.57442742, 128.1593441'
-	address = loc_trans(location)
-	print(address)
-	assert isinstance(address, str)
+    location = "36.57442742, 128.1593441"
+    address = loc_trans(location)
+    print(address)
+    assert isinstance(address, str)
